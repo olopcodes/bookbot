53d4270a1756778bf0cc33158c01cc2cd26930f3
olopcodes <olopcodes@gmail.com>
2025-10-30 18:45:54 -0400

    D: added num_of_words function

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 def get_book_text(file_path):
-    # print(file_path);
     with open(file_path) as f:
         file_contents = f.read();
-        print(file_contents);
+        return file_contents;        
+        # print(file_contents);
+
+
+def get_num_of_words(book_text):
+    count = 0;
+    arr_text = book_text.split();
+    # one way to solve this 
+    # for text in len(arr_text):
+    #     count += 1;
+    # return count;
+
+    # another way to solve this
+    for text in range(len(arr_text) - 1):
+        count += 1;  
+    return count;
+
 
 def main():
-    get_book_text("books/frankenstein.txt");
+    # text of book
+    book_text = get_book_text("books/frankenstein.txt");
+    # get the num of words
+    num_of_words = get_num_of_words(book_text);
+    print(f"Found {num_of_words} total words");
+
 
 main();
