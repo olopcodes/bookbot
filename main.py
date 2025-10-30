74c1f9c8afc9f6e99a7a42b07ad730f17b2fdd21
olopcodes <olopcodes@gmail.com>
2025-10-29 21:59:07 -0400

    C: added get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots");
+def get_book_text(file_path):
+    # print(file_path);
+    with open(file_path) as f:
+        file_contents = f.read();
+        print(file_contents);
+
+def main():
+    get_book_text("books/frankenstein.txt");
+
+main();
